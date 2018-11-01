83c6be3ee581f89a083137d7873657e290f5f1b0
Potatoman <s10174226@connect.np.edu.sg>
2018-11-01 22:23:31 +0800

    sending a post request works

--- db/index.js
+++ db/index.js
@@ -1,21 +1,23 @@
-let fs = require('fs');
-let path = require('path');
-let dbPath = path.join('db', 'users.json');
+let fs = require("fs");
+let path = require("path");
+let dbPath = path.join("db", "users.json");
 
 const userSchema = {
-    users: {
-        id: '',
-        'user-nric': '',
-        nrics: [],
-    },
+    "users": [
+        {
+        "id": "",
+        "user-nric": "",
+        "nrics": [],
+    }
+],
 };
 const initialise = () => {
     // checks if db exists
     if (!fs.existsSync(dbPath)) {
         seed(dbPath);
-        console.log('file has been created and loaded');
-    }else{
-        console.log('file already exists no need to create');
+        console.log("file has been created and loaded");
+    } else {
+        console.log("file already exists no need to create");
     }
 };
 
--- src/app.js
+++ src/app.js
@@ -3,18 +3,22 @@ let app = express();
 let jsonServer = require('json-server');
 let fs = require('fs');
 let db = require('../db');
+let path = require('path');
 
 //init db
 db.initialise();
+
+//route to user.json
 const userEndpoint = jsonServer.router('db/users.json');
 
-app.use(express.static('db.json'));
-app.use('/users', userEndpoint);
+//app.use(express.static(path.join(__dirname, '../db/user.json')));
 
-app.get('/', function(res, req) {
-    req.redirect('/data/employees');
+app.get('/', function (req, res) {
+    res.redirect('/users');
 });
 
 app.listen(3000, () => {
     console.log('You are listening on 3000');
 });
+
+app.use(userEndpoint);
