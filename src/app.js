4c162ee1d05d9784c0e13cd5ac9f38cbf571bca7
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2018-11-07 17:30:29 +0800

    Add db helper methods

--- src/app.js
+++ src/app.js
@@ -12,6 +12,15 @@ app.get("/", (req, res) => {
     res.send(200);
 });
 
+// app.post("/examplePOST", (req, res) => {
+//     console.log(req.body.data);
+//     db.push("example", req.body.data);
+//     db.getList("example", (err, result) => {
+//         console.log(result);
+//         res.send(JSON.stringify(result));
+//     });
+// });
+
 app.listen(process.env.PORT, () => {
-    console.log("App running on port 3000");
+    console.log("⚡️ - Server running on port 3000");
 });
--- src/db/index.js
+++ src/db/index.js
@@ -3,12 +3,34 @@ let redis =
     process.env.ENV === "dev" ? new Redis() : new Redis(process.env.REDIS_URL);
 
 const initialise = () => {
-    redis.set("foo", "bar");
-    redis.get("foo", (err, result) => {
-        console.log(result);
-    });
+    if (process.env.ENV === "dev") redis.flushdb();
+};
+
+const set = (key, data) => redis.set(key, value);
+const get = (key, callback) => redis.get(key, callback);
+
+// List operations
+const pushList = async (key, data) => {
+    try {
+        const result = await redis.rpush(key, JSON.stringify(data));
+    } catch (err) {
+        console.log(err);
+    }
+};
+const getList = async (key, callback, limit = 0) => {
+    try {
+        const result = await redis.lrange(key, 0, limit - 1);
+        callback(null, result);
+    } catch (err) {
+        console.log(err);
+        callback(err, null);
+    }
 };
 
 module.exports = {
     initialise,
+    set,
+    get,
+    pushList,
+    getList,
 };
